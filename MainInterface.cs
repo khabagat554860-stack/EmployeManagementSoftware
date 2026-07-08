167e8e01e800b52f042212b9feba1847a9f835c5
Kimbap-08 <y.noh.554158@umindanao.edu.ph>
2026-07-08 15:14:23 +0800

    added salary, employees, sign out label and icon

--- MainInterface.cs
+++ MainInterface.cs
@@ -22,37 +22,24 @@ namespace EmployeManagementSoftware
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void MainInterface_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
+        private void lblDashboard_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void lblEmployees_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStaffroom_Click(object sender, EventArgs e)
         {
 
         }
